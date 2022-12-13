19c0778e6bbcb21625a2159a481f14d82112fa6a
shehab267 <shehabelhawary267@gmail.com>
2022-12-13 10:17:48 +0200

    Sending a PUT Http Request to Store Coach Data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,14 +1,30 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
-      areas: data.areas
+      areas: data.areas,
     };
 
-    context.commit('registerCoach', coachData);
-  }
+    const response = await fetch(
+      `https://vue-demo-coaching-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+    // const responseData = await response.json();
+    // Catching errors
+    if (!response.ok) {
+      // throw error
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
+  },
 };
