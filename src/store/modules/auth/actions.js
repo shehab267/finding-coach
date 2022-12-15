7b953a3bbe06b0bf1fae4ce5b60c1b631edb25a7
shehab267 <shehabelhawary267@gmail.com>
2022-12-15 22:36:10 +0200

    Restructure (auth action.js) code

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,32 +1,60 @@
 export default {
   async login(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBb4jsIgh9_iDeKHjVOnzXwJq_Bs-4jaX8',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+    return context.dispatch('auth', {
+      // copy existing payload
+      ...payload,
+      mode: 'login',
+    });
+  },
+  async signup(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
+
+  async auth(context, payload) {
+    //  mode = (login | signup)
+    const mode = payload.mode;
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBb4jsIgh9_iDeKHjVOnzXwJq_Bs-4jaX8';
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBb4jsIgh9_iDeKHjVOnzXwJq_Bs-4jaX8';
+    }
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
     const responseData = await response.json();
     if (!response.ok) {
-      console.log(responseData);
       let msg = null;
-
-      if (responseData.error.message === 'INVALID_PASSWORD') {
+      // SignUp Errors
+      if (responseData.error.message === 'EMAIL_EXISTS') {
+        msg = 'The email address is already in use by another account';
+      } else if (responseData.error.message === 'OPERATION_NOT_ALLOWED') {
+        msg = ' Password sign-in is disabled for this project';
+      } else if (responseData.error.message === 'TOO_MANY_ATTEMPTS_TRY_LATER') {
+        msg =
+          'We have blocked all requests from this device due to unusual activity. Try again later';
+      } // Login Errors
+      else if (responseData.error.message === 'INVALID_PASSWORD') {
         msg = 'The password is invalid';
       } else if (responseData.error.message === 'EMAIL_NOT_FOUND') {
         msg = 'The Email is invalid';
       } else if (responseData.error.message === 'USER_DISABLED') {
         msg = 'The user account has been disabled by an administrator';
       }
+
       const error = new Error(`Faild to authenticate, ${msg}!`);
+      // const error = new Error(response.message || 'Faild to authenticate!');
       throw error;
     }
-    console.log(responseData);
+
     // Commit the response at mutations and pass appropriate payload obj
     context.commit('setUser', {
       token: responseData.idToken,
@@ -34,44 +62,7 @@ export default {
       tokenExpiration: responseData.expiresIn,
     });
   },
-  async signup(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBb4jsIgh9_iDeKHjVOnzXwJq_Bs-4jaX8',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
-    const responseData = await response.json();
-    if (!response.ok) {
-      console.log(responseData);
-      let msg = null;
-      // const error = new Error(response.message || 'Faild to authenticate!');
-      if (responseData.error.message === 'EMAIL_EXISTS') {
-        msg = 'The email address is already in use by another account';
-      }
-      if (responseData.error.message === 'OPERATION_NOT_ALLOWED') {
-        msg = ' Password sign-in is disabled for this project';
-      } else if (responseData.error.message === 'TOO_MANY_ATTEMPTS_TRY_LATER') {
-        msg =
-          'We have blocked all requests from this device due to unusual activity. Try again later';
-      }
-      const error = new Error(`Faild to authenticate, ${msg}!`);
-      // const error = new Error(response.message || 'Faild to authenticate!');
-      throw error;
-    }
-    console.log(responseData);
-    // Commit the response at mutations and pass appropriate payload obj
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
-    });
-  },
+
   logout(context) {
     context.commit('setUser', {
       token: null,
