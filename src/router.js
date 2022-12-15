8fb8d850a90b43180b47091ab452d8301ad22462
shehab267 <shehabelhawary267@gmail.com>
2022-12-15 19:24:35 +0200

    Authentication & Routing (incl. Navigation Guards)

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import RequestsList from './pages/requests/RequestsList.vue';
 import NotFound from './pages/NotFound.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
+import store from './store/index.js';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -21,11 +22,25 @@ const router = createRouter({
         { path: 'contact', component: ContactForm }, // /coaches/a1/contact "Contact with a specific coach"
       ],
     },
-    { path: '/register', component: CoachRegistration },
-    { path: '/request', component: RequestsList },
-    { path: '/auth', component: UserAuth },
+    {
+      path: '/register',
+      component: CoachRegistration,
+      meta: { requiresAuth: true },
+    },
+    { path: '/request', component: RequestsList, meta: { requiresAuth: true } },
+    { path: '/auth', component: UserAuth, meta: { requiresUnAuth: true } },
     { path: '/:notFound(.*)', component: NotFound },
   ],
+});
+
+router.beforeEach(function (to, _, next) {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next('/auth');
+  } else if (to.meta.requiresUnAuth && store.getters.isAuthenticated) {
+    next('/coaches');
+  } else {
+    next();
+  }
 });
 
 export default router;
